00e15e0dfaf6cd4d2832e4742f8c3b645aa5f8a6
Your Name <you@example.com>
2019-11-21 20:18:03 +0800

    11.21完成函数

--- web_back/js/user/user.js
+++ web_back/js/user/user.js
@@ -6,5 +6,21 @@ var user = {
                 location.href = './login.html'
             }
         })
+    },
+    login: function(myName, myPassword) {
+        $.post('http://localhost:8000/admin/login', {
+                user_name: myName,
+                password: myPassword
+            },
+            function(res) {
+                console.log(res);
+                if (res.code === 200) {
+                    // alert('登陆成功')
+                    location.href = './index.html'
+                } else {
+                    // alert（'res.msg'）
+                    alert(res.msg)
+                }
+            })
     }
 }
