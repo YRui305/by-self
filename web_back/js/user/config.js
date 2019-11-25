e2475098f6c1b91f731a3c1d45eac354a0a1cd9c
Your Name <you@example.com>
2019-11-25 10:30:22 +0800

    11.25登录页面和用户界面

---
+++ web_back/js/user/config.js
@@ -0,0 +1,5 @@
+var baseUrl = 'http://localhost:8000/'
+
+var APIURLS={
+    category_show:baseUrl+'admin/category_search'
+}
--- web_back/js/user/user.js
+++ web_back/js/user/user.js
@@ -1,37 +1,40 @@
 var baseUrl = 'http://localhost:8000/'
 var user = {
-    logout: function() {
+    logout: function(callback) {
         $.post(baseUrl + 'admin/logout', function(res) {
             // console.log(res);
-            if (res.code === 200) {
-                location.href = './login.html'
-            }
+            callback(res)
+                // if (res.code === 200) {
+                //     location.href = './login.html'
+                // }
         })
     },
-    login: function(myName, myPassword) {
+    login: function(myName, myPassword, callback) {
         $.post(baseUrl + 'admin/login', {
                 user_name: myName,
                 password: myPassword
             },
             function(res) {
-                console.log(res);
-                if (res.code === 200) {
-                    // alert('登陆成功')
-                    location.href = './index.html'
-                } else {
-                    // alert（'res.msg'）
-                    alert(res.msg)
-                }
+                callback(res)
+                    // console.log(res);
+                    // if (res.code === 200) {
+                    //     // alert('登陆成功')
+                    //     location.href = './index.html'
+                    // } else {
+                    //     // alert（'res.msg'）
+                    //     alert(res.msg)
+                    // }
             })
     },
-    getInfo: function() {
+    getInfo: function(callback) {
         // $.get(地址，参数，回调)
         $.get(baseUrl + 'admin/getuser', function(res) {
-            console.log(res);
-            if (res.code === 200) {
-                $("#userImg").prop('src', res.data.user_pic)
-                $('#userName').text(res.data.nickname)
-            }
+            callback(res)
+                // console.log(res);
+                // if (res.code === 200) {
+                //     $(".userImg").prop('src', res.data.user_pic)
+                //     $('#userName').text(res.data.nickname)
+                // }
 
         })
     }
